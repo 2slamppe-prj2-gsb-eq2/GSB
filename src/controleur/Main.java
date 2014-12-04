2b254c688ed92abdda75da9397057e3cfcafba92
btssio <btssio@btssio-VirtualBox>
2014-12-04 14:53:24 +0100

    Création Package Controleur + création du main + création du Controleur principal + création du modèle jdbc

---
+++ src/controleur/Main.java
@@ -0,0 +1,36 @@
+package controleur;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.Properties;
+import javax.swing.JOptionPane;
+
+
+/**
+ * Ambulances
+ *
+ * @version v2 21 novembre 2013
+ * @author nbourgeois 
+ * Objectif : 
+ * - exemple de dynamique Vue/Controleur avec controleur principal
+ * - exemple de pattern Dao
+ * - exemple de singleton de connexion à un SGBD
+ * - exemple de paramétrage utilisant un fichier de "properties"
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args)  {
+
+        String ficPropertiesJdbc  ; // nom du fichier de properties
+        Properties propertiesJdbc;                              // objet de propriétés (paramètres de l'appplication) pour Jdbc
+        FileInputStream input;                                  // flux de lecture des properties
+        CtrlPrincipal ctrlPrincipal;                                  // référence vers le contrôleur principal
+
+    }
+}
+    
